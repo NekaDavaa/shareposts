abceafe6faa805c5c5bfc3306e1f1381567fe99e
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-12-21 10:35:20 +0200

    Users auth system

---
+++ app/views/users/register.php
@@ -0,0 +1,9 @@
+<?php require HEADER; ?>
+  <div class="row">
+    <div class="col-md-6 mx-auto">
+      <div class="card card-body bg-light mt-5">
+        <h2>Create An Account</h2>
+      </div>
+    </div>
+  </div>
+<?php require FOOTER; ?>
