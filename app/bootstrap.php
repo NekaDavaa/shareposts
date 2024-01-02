5e55967286d326b9ef55e911206b8be1170c67f3
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2024-01-02 15:18:54 +0200

    asd

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,7 @@
 require_once 'config/config.php';
 //Load Helpers Functions
 require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
 
 //Autoloader
 function my_autoloader($class_name) {
